bf378dee321e7c030e9e62cb028d0f38911c4b22
Erin Byrd <your@primary.email>
2022-02-19 23:42:51 -0500

    second push

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request
-from helpers import user, find_house
+from helpers import user, find_house, covid_result, find_sleep, find_water_intake
 import http.client
 import json
 
 app: Flask = Flask(__name__)
 
-
 users: list[user] = []
 user_number: int = 0
 
 # summary to be texted/result: 
-summary: str = ("Summary report: \n Idk \n 1234")
+recommended_sleep: int = 0
+recommended_water: int = 0
+water_message: str = f"Recommended Water Intake: {recommended_water} cups"
+sleep_message: str = f"Recommended Sleep: {recommended_sleep} hours"
+doctor_message: str = "It is recommended you go see a doctor." 
+
+summary: str = (f"Summary report: ")
 
 @app.route("/")
 def index():
@@ -29,18 +34,59 @@ def display_user(usernumber: str):
 
 @app.route('/resources')
 def resources():
+    if request.method == "post":
+        global users 
+        global user_number
+        phone: str = ""
+        covid: str = request.form['COVID']
+        # message: str = ""
+
+        covid_message: str = covid_result(covid)
+
+        # here is the bandwidth API 
+        conn = http.client.HTTPSConnection("messaging.bandwidth.com")
+        if covid_message == "no":
+            payload = json.dumps({
+            "to": [
+                f"+1{phone}"
+            ],
+            "from": "+19193913463",
+            "text": "You have opted not to receive COVID updates. Have a good day :)",
+            "media": "https://www1.pictures.livingly.com/mp/81ln60YcYdil.jpg",
+            "tag": "covid message"
+            })
+        else:        
+            payload = json.dumps({
+            "to": [
+                f"+1{phone}"
+            ],
+            "from": "+19193913463",
+            "text": "As of 12:05pm of February 18th, 2020:\nNewly Reported Cases: 4,871\nCurrently Hospitalized: 2,634\nDeaths: 22,148 deaths\nDaily Percent Positive: 10.3%\n75% of the adult population is vaccinated with at least one dose\nReminder to wear a mask, wash your hands, get vaccinated, and take other necessary health precautions<3",
+            "applicationId": "1955b5c0-a0c0-498e-955a-719093718d6c",
+            "media": "https://www1.pictures.livingly.com/mp/81ln60YcYdil.jpg",
+            "tag": "covid message"
+            })
+        headers = {
+        'Authorization': 'Basic aGFja2F0aG9uXzAxOmYqdGdlcEd4QnlHVnRFQ1cmVzdXR2YzJFJ3Z1BNSA==',
+        'Content-Type': 'application/json'
+        }
+        conn.request("POST", "/api/v2/users/5008573/messages", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+        # end Bandwidth API
     return render_template('resources.html')
 
 
-@app.route('/quiz', methods=["GET", "POST"])
-def quiz():
+@app.route('/survey', methods=["GET", "POST"])
+def survey():
     if request.method == "POST":
         global users
         global user_number
 
         fname: str = request.form['fname']
         lname: str = request.form['lname']
-        animal: str = request.form['animal']
+        # animal: str = request.form['animal']
         phone: str = ""
         for character in request.form['number']:
             try: 
@@ -53,13 +99,21 @@ def quiz():
         # TODO: store other inputs as variables
 
         if fname == '' or lname == '':
-            return render_template("quiz.html")
+            return render_template("survey.html")
 
-        house: str = find_house(animal)
-        new_user: user = user(user_number, fname, lname, house)
+        # house: str = find_house(animal)
+        new_user: user = user(user_number, fname, lname, summary)
         users.append(new_user)
 
         user_number += 1
+
+        # summary formatting
+        # if "most important info" == "water":
+            # summary = ""
+        # if doctor
+        if request.form['symptoms'] != 'none': 
+            summary = summary + doctor_message 
+
         # here is the bandwidth API 
         conn = http.client.HTTPSConnection("messaging.bandwidth.com")
         payload = json.dumps({
@@ -69,7 +123,7 @@ def quiz():
         "from": "+19193913463",
         "text": summary,
         "applicationId": "1955b5c0-a0c0-498e-955a-719093718d6c",
-        "media": "https://static.wikia.nocookie.net/club-penguin-rewritten/images/9/95/Water_Droplet_Pin.png/revision/latest/top-crop/width/360/height/450?cb=20170720010752?crop=1xw:1xh;center,top&resize=768:*",
+        "media": "https://i.imgur.com/pV8dKER.png",
         "tag": "test message"
         })
         headers = {
@@ -81,8 +135,8 @@ def quiz():
         data = res.read()
         print(data.decode("utf-8"))
         # end Bandwidth API
-        return render_template("result.html", house=house)
-    return render_template("quiz.html")
+        return render_template("result.html", summary=summary)
+    return render_template("survey.html")
 
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -11,6 +11,44 @@ class user:
         self.house = house
 
 
+def find_water_intake(age: int) -> str:
+    """Based on the users age, returns the recommended amount of water intake in oz."""
+    if age < 4:
+        return "16"
+    elif age <= 8:
+        return "40"
+    elif age <= 13: 
+        return "56-64"
+    elif age <= 18:
+        return "64-88"
+    else:
+        return "72-104"
+
+
+def find_sleep(age: int) -> str:
+    if age < 1:
+        return "13-16"
+    elif age == 1 or age == 2:
+        return "11-14"
+    elif age == 3 or age == 4 or age == 5:
+        return "10-13"
+    elif age >= 6 and age <= 13:
+        return "9-12"
+    elif age >= 14 and age <= 17:
+        return "8-10"
+    elif age >= 18 and age <= 64:
+        return "7-9"
+    else:
+        return "7-8"
+
+
+def doctor_or_not(symptoms: bool) -> bool:
+    if symptoms:
+        return True
+    else: 
+        return False
+    
+
 def find_house(animal: str) -> str:
     if animal == 'eagle':
         return 'Ravenclaw'
@@ -20,3 +58,10 @@ def find_house(animal: str) -> str:
         return 'Hufflepuff'
     else:
         return 'Slytherin'
+
+
+def covid_result(covid: str) -> str:
+    if covid == 'yes':
+        return "yes"
+    elif covid == 'no':
+        return "no"
